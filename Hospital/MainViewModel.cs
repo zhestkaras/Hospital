8f1d68063d34a5c4ff6f60b9a68d228aebc1ff15
user <user@CAB1-10>
2025-01-30 10:20:32 +1000

    гншнш

---
+++ Hospital/MainViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+namespace Hospital
+{
+    public partial class MainWindow
+    {
+        public class MainViewModel : INotifyPropertyChanged
+        {
+            private PatientDB patientDB = new PatientDB();
+            private Patient editPatient = new Patient();
+            public ObservableCollection<Patient> Patients => patientDB.Patients;
+
+            public Patient EditPatient
+            {
+                get => editPatient;
+                set
+                {
+                    editPatient = value;
+                    OnPropertyChanged(nameof(EditPatient));
+
+                }
+            }
+
+
+
+            public ICommand AddPatientCommand { get; }
+
+            public MainViewModel()
+            {
+                AddPatientCommand = new CommandVM(AddPatient);
+            }
+
+            private void AddPatient()
+            {
+                if (ValidatePatient(EditPatient))
+                {
+                    patientDB.AddPatient(new Patient
+                    {
+                        FullName = EditPatient.FullName,
+                        DateOfBirth = EditPatient.DateOfBirth,
+                        Gender = EditPatient.Gender,
+                        MedicalCardNumber = EditPatient.MedicalCardNumber,
+                        Diagnosis = EditPatient.Diagnosis,
+                        AdmissionDate = DateTime.Now
+                    });
+
+
+                    EditPatient = new Patient();
+                }
+                else
+                {
+                    MessageBox.Show("Error");
+                }
+            }
+
+            private bool ValidatePatient(Patient patient)
+            {
+
+                return !string.IsNullOrWhiteSpace(patient.FullName) &&
+                       !string.IsNullOrWhiteSpace(patient.MedicalCardNumber) &&
+                       patientDB.IsMedicalCardNumberUnique(patient.MedicalCardNumber);
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            protected void OnPropertyChanged(string propertyName)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+           
+            }
+
+        }
+
+    }
+
+
+
+
+
+
--- Hospital/MainWindow.xaml.cs
+++ Hospital/MainWindow.xaml.cs
@@ -1,18 +1,13 @@
-﻿using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.ComponentModel.Design;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 namespace Hospital
 {
     /// <summary>
@@ -21,93 +16,17 @@ namespace Hospital
     public partial class MainWindow : Window
     {
 
-
-        public class MainViewModel : INotifyPropertyChanged
-        {
-            private PatientDB patientDB = new PatientDB();
-            private Patient editPatient = new Patient();
-            public ObservableCollection<Patient> Patientss { get; set; }
-            public ObservableCollection<Patient> Patients => patientDB.Patients;
-
-            public Patient EditPatient
-            {
-                get => editPatient;
-                set
-                {
-                    editPatient = value;
-                    OnPropertyChanged(nameof(EditPatient));
-                   
-                }
-            }
-
-            public ICommand AddPatientCommand { get; }
-
-            public MainViewModel()
-            {
-                AddPatientCommand = new CommandVM(AddPatient);
-            }
-
-            private void AddPatient()
-            {
-                if (ValidatePatient(EditPatient))
-                {
-                    patientDB.AddPatient(new Patient
-                    {
-                        FullName = EditPatient.FullName,
-                        DateOfBirth = EditPatient.DateOfBirth,
-                        Gender = EditPatient.Gender,
-                        MedicalCardNumber = EditPatient.MedicalCardNumber,
-                        Diagnosis = EditPatient.Diagnosis,
-                        AdmissionDate = DateTime.Now
-                    });
-
-                   
-                    EditPatient = new Patient();
-                }
-                else
-                {
-                    //  добавить обработку ошибок валидации
-                }
-            }
-
-            private bool ValidatePatient(Patient patient)
-            {
-                // Проверка 
-                return !string.IsNullOrWhiteSpace(patient.FullName) &&
-                       !string.IsNullOrWhiteSpace(patient.MedicalCardNumber) &&
-                       patientDB.IsMedicalCardNumberUnique(patient.MedicalCardNumber);
-            }
-
-            public event PropertyChangedEventHandler PropertyChanged;
-
-            protected void OnPropertyChanged(string propertyName)
-            {
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-            }
-            private void Button_Click(object sender, RoutedEventArgs e)
-            {
-                if (EditPatient != null)
-                {
-                    ///////////////////sdfgv;ozxvn;oxzihvdl;z
-                    EditPatient.Patients.Add(new Patient { FullName = "Новая проблема", Gender = "Описание проблемы", MedicalCardNumber = "Заявитель", Diagnosis = "Заявитель" });
-                }
-            }
-        }
-
-
-
         public MainWindow()
         {
             InitializeComponent();
 
             DataContext = new MainViewModel();
 
-            DataContext = this;
         }
 
-        
     }
 }
+
 
 
 
